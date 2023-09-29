e0679d8d31cde6ecdb5d8a946b40b9a1fb65daad
Junseo Oh <40460655+outsung@users.noreply.github.com>
2023-09-29 15:59:25 +0900

    Feat: add article interfaces

--- src/interfaces/article.ts
+++ src/interfaces/article.ts
@@ -1,3 +1,23 @@
-export interface IArticle {
-    
+export interface ArticleList extends Omit<Article, "contents" | "comments"> {
+  summary: string;
+  commentCount: number;
+}
+
+export interface Article {
+  title: string;
+  author: User;
+  contents: string;
+  comments: Comment[];
+  createdAt: string;
+}
+
+export interface Comment {
+  contents: string;
+  author: User;
+  createdAt: string;
+}
+
+export interface User {
+  name: string;
+  profileImage?: string;
 }
