d45d050395a528c29619c7ca24a3ccc517f3167f
Taegyu Lee <meleeisdeveloping@gmail.com>
2023-10-01 11:24:34 +0000

    create apiMock & working on page.tsx of category...

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,17 +1,44 @@
-import Home from "../page";
 import { articlesMock } from "@/utils";
 import { ListFilter } from "@/components/article";
 import { ArticleList } from "@/components/article";
+import { getCategoryDetail } from "@/utils/apiMock";
+import type { InferGetStaticPropsType, GetStaticProps, GetStaticPropsContext } from 'next'
+
+interface ICategoryParamsProps {
+   category: string;
+   detail: {
+    name: string
+    key: string
+  } 
+}
 
 interface ICategoryProps {
-  params: { category: string };
-  searchParams: {
+    params: ICategoryParamsProps
+    searchParams: {
     filter: string;
     [key: string]: string | string[] | undefined;
   };
 }
+// export async function getStaticProps(context: GetStaticPropsContext<{ category: string }>) {
+//   if (context.params?.category) {
+//     const detail = getCategoryDetail(context.params?.category)
+//     return {
+//       props: { detail }
+//     }
+//   }
+// }
+// export const getStaticProps = (async (context: GetStaticPropsContext<{category: string}>) => {
+//   if (context.params?.category) {
+//         const detail = getCategoryDetail(context.params?.category as string)
+//         return {
+//           props: { detail }
+//         }
+//       }
+// })
+
 
 export default function Category({ params, searchParams }: ICategoryProps) {
+  
   const filter = [
     {
       name: "화제",
@@ -25,7 +52,7 @@ export default function Category({ params, searchParams }: ICategoryProps) {
 
   return (
     <div className="flex flex-col first-letter:h-full bg-background-green">
-      <div className="">{params.category}</div>
+      <div className="text-7xl">{params.detail.name}</div>
       <ListFilter
         items={filter}
         selectedItem={(searchParams.filter) || filter[0].key}
