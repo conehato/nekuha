f73690b64f9c885f549e26b42684169fac46238d
Taegyu Lee <meleeisdeveloping@gmail.com>
2023-10-02 13:56:04 +0000

    Resized Header icon size

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export function Header() {
   return (
     <div className="flex flex-row w-screen px-4 py-1.5 justify-between items-center border-b-2 border-main-blue-dark">
-      <Bars3Icon className="w-8 h-8" />
+      <Bars3Icon className="w-7 h-7" />
       <Link href="/">
         <div className="text-2xl font-bold my-auto pb-1 justify-center">
           {names.serviceName}
         </div>
       </Link>
-      <MagnifyingGlassIcon className="w-8 h-8" />
+      <MagnifyingGlassIcon className="w-7 h-7" />
     </div>
   );
 }
